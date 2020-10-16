e9464c174f67c73e75dcad0888c0a8d509b104c9
Guillermo Mora <guillermo_moram@outlook.es>
2020-10-16 14:50:46 -0500

    docs agregado

---
+++ docs/precache-manifest.96bca21d4db3b06460ded8401039a617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432b0218b2442ad6a7860e3b04925acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d0ee55eeca68256d3d",
+    "url": "/static/css/main.72b3cefc.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d0ee55eeca68256d3d",
+    "url": "/static/js/main.fc85e893.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
